a323ac9088a8d6687333bba68aab0a1f8925a5fa
Tomohiko Himura <eiel.hal@gmail.com>
2018-01-13 22:39:41 +0900

    code format

--- lib/endpoint/base.ex
+++ lib/endpoint/base.ex
@@ -1,9 +1,8 @@
 defmodule ChatworkEx.Endpoint.Base do
-
   alias ChatworkEx.HeaderCreator
   alias ChatworkEx.ParameterNormalizer
   alias ChatworkEx.Response
-   alias ChatworkEx.Response.RateLimit
+  alias ChatworkEx.Response.RateLimit
   alias ChatworkEx.Response.Error
   alias ChatworkEx.Errors.BadClientError
   alias ChatworkEx.Errors.UnauthorizedError
@@ -14,10 +13,13 @@ defmodule ChatworkEx.Endpoint.Base do
 
   def get!(url, access_token, options \\ []) do
     headers = HeaderCreator.headers(access_token)
-    url = case options do
-      [] -> url
-      _ -> url <> "?" <> URI.encode_query(options)
-    end
+
+    url =
+      case options do
+        [] -> url
+        _ -> url <> "?" <> URI.encode_query(options)
+      end
+
     HTTPoison.get!(url, headers)
   end
 
@@ -38,41 +40,43 @@ defmodule ChatworkEx.Endpoint.Base do
   end
 
   def parse_header({"X-RateLimit-Limit", limit}, rate_limit), do: %{rate_limit | limit: limit}
-  def parse_header({"X-RateLimit-Remaining", remaining}, rate_limit), do: %{rate_limit | remaining: remaining}
-  def parse_header({"X-RateLimit-Reset", reset}, rate_limit), do: %{ rate_limit | reset: reset }
+
+  def parse_header({"X-RateLimit-Remaining", remaining}, rate_limit),
+    do: %{rate_limit | remaining: remaining}
+
+  def parse_header({"X-RateLimit-Reset", reset}, rate_limit), do: %{rate_limit | reset: reset}
   def parse_header(_, rate_limit), do: rate_limit
 
   def to_response!(
-    %HTTPoison.Response{ status_code: 200, body: body, headers: headers},
-    struct
-  ) do
+        %HTTPoison.Response{status_code: 200, body: body, headers: headers},
+        struct
+      ) do
     body = Poison.decode!(body, as: struct)
     rate_limit = List.foldl(headers, %RateLimit{}, &parse_header/2)
-    %Response{ rate_limit: rate_limit, body: body }
+    %Response{rate_limit: rate_limit, body: body}
   end
 
   def to_response!(
-    %HTTPoison.Response{ status_code: 204, headers: headers },
-    _struct
-  ) do
+        %HTTPoison.Response{status_code: 204, headers: headers},
+        _struct
+      ) do
     rate_limit = List.foldl(headers, %RateLimit{}, &parse_header/2)
-    %Response{ rate_limit: rate_limit, body: nil }
+    %Response{rate_limit: rate_limit, body: nil}
   end
 
   def to_response!(
-    %HTTPoison.Response{ status_code: 401, body: body },
-    _struct
-  ) do
-    error = Poison.decode! body, as: %Error{}
+        %HTTPoison.Response{status_code: 401, body: body},
+        _struct
+      ) do
+    error = Poison.decode!(body, as: %Error{})
     raise UnauthorizedError, message: error
   end
 
   def to_response!(
-    %HTTPoison.Response{ status_code: 403, body: body },
-    _struct
-  ) do
-    error = Poison.decode! body, as: %Error{}
+        %HTTPoison.Response{status_code: 403, body: body},
+        _struct
+      ) do
+    error = Poison.decode!(body, as: %Error{})
     raise BadClientError, message: error
   end
-
 end
